a7b1cd717b36c707e722cf51bb82d3c64b673636
Bryan Austyn Ichsan <101229576@students.swinburne.edu.my>
2021-10-03 11:46:39 +0700

    Added Sass module into the project

--- sendika-app/src/App.js
+++ sendika-app/src/App.js
@@ -1,10 +1,12 @@
-import './assets/css/App.css';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import './assets/css/App.scss';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar/>
         <Switch>
           <Route exact path='/'>
 
---
+++ sendika-app/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import "../assets/css/components/NavBar.scss"
+
+
+// Sorry lupa kalau ini bagian kau 
+// Jadi itu lah aku comment kan biar kau modify 
+function NavBar() {
+    return (
+    <div>
+        
+        {/* <Link to="/">Sendika</Link>
+        
+
+        <div class="links">
+            
+            <Link to="/">Home</Link>
+
+            <Link to="/upload">Upload</Link>
+
+            <Link to="/about">About</Link>
+
+        </div> */}
+    </div>
+    )
+}
+
+export default NavBar
