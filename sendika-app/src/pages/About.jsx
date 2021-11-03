6dcbe076cefecaee9ae35a7343a2a83612c14396
tensan8 <tensanf@gmail.com>
2021-11-03 13:33:54 +0700

    edit about page

--- sendika-app/src/pages/About.jsx
+++ sendika-app/src/pages/About.jsx
@@ -34,8 +34,7 @@ function About() {
                         &apos;pIC50 Hunters&apos; is the name of the team that was made by Alesandro and Bryan under the supervision of Dr. Lee Sue Han, Dr. Xavier Wezen Chee, and Dr. Joel Chia Ming Than.
                         <br/><br/>Participating in their first competition in making innovation by using Data, Alesandro and Bryan have decided to make an AI model by utilizing the SMILES dataset. 
                         This project was made as one of the deliverables that this team will use to show the innovation in a website integrated with Artificial Intelligence.
-                        <br/><br/>Sendika will help researchers to predict the pIC50 value of a compound; 
-                        hence they can cut the time that they should spend in order to test whether a particular compound can be an inhibitor or not by having a prior prediction on how possible the compound will be an inhibitor, predicted by the AI.
+                        <br/><br/>Sendika will help researchers to predict the pIC50 value of a compound. It might help researchers to predict potential inhibitors of HIV-1 integrase.
                     </p>
                 </div>
 
